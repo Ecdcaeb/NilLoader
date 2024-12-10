6facac00a7532e09d05baf87c50d2411023441c0
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2024-12-10 23:14:42 +0800

    we do not have to use it, it will be packaged when build

--- src/lib/java/nilloader/api/lib/nanojson/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Lightweight JSON input/output library. 
- * 
- * For reading JSON, see {@link nilloader.api.lib.nanojson.JsonParser}. For writing JSON, see 
- * {@link nilloader.api.lib.nanojson.JsonWriter}.
- */
-package nilloader.api.lib.nanojson;
-
